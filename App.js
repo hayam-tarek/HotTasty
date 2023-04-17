81b27215c3c786af5137a3827094680433d0c148
Hayam <hayamtareq28@gmail.com>
2023-04-17 16:18:47 +0200

    last

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './page/home';
-import Register from './page/register';
-import SignIN from './page/signIN';
-import Profile from './page/profile';
-import GetStart from './page/getStart';
-import ForgetPass from './page/forgetPass';
 import Index from "./routes/index";
-
-//const Stack = createNativeStackNavigator();
 
 export default function App() {
 
